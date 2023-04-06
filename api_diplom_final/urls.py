d326af72ae47d9f9e743f4076f3f17df24e82312
Oleg_Kotov <moto198720@yandex.ru>
2023-04-06 16:24:34 +0300

    In process

---
+++ api_diplom_final/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+
+urlpatterns = [
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('', include('usermanager.urls')),
+    path('', include('shopmanager.urls')),
+    path('', include('ordermanager.urls')),
+]
